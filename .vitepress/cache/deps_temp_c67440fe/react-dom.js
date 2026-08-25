2e62ba20fb1884f45bd2728bdc4ddbc267b2dfb2
Artem Pisarev <artem@pisarev.in>
2026-08-25 11:09:19 +0300

    fix: stop tracking the VitePress dependency cache
    It landed in the previous commit — 53k lines of prebundled deps that are
    regenerated on every build.

--- .vitepress/cache/deps_temp_c67440fe/react-dom.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-L3FESE7A.js";
-import "./chunk-F46FA3OT.js";
-import "./chunk-BUSYA2B4.js";
-export default require_react_dom();
-//# sourceMappingURL=react-dom.js.map
--- .vitepress/cache/deps_temp_c67440fe/react.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_react
-} from "./chunk-F46FA3OT.js";
-import "./chunk-BUSYA2B4.js";
-export default require_react();
-//# sourceMappingURL=react.js.map
